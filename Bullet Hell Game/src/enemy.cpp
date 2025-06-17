08360e6d675bda7e2fdbe8e9fa396788f3912c0a
Diego Barrales <dbarrales06@gmail.com>
2025-06-17 14:15:44 -0700

    Added Enemy Path Variation by giving the enemies a X Velocity

--- Bullet Hell Game/src/enemy.cpp
+++ Bullet Hell Game/src/enemy.cpp
@@ -2,15 +2,23 @@
 
 static constexpr Color enemy_color = { 255, 50, 50, 255 };
 
-static constexpr Vector2 default_velocity = { 0, 500 };
-static constexpr int min_velocity_range = 550;
-static constexpr int max_velocity_range = 750;
+static constexpr Vector2 default_velocity = { 300, 500 };
+static constexpr int min_speed_x_range = 250;
+static constexpr int max_speed_x_range = 350;
+static constexpr int min_speed_y_range = 550;
+static constexpr int max_speed_y_range = 750;
 static constexpr float hitbox_radius = 35.0f;
 
 namespace bts
 {
     Enemy::Enemy(Vector2 start_position) : GameObject(start_position, default_velocity, Hitbox::CIRCLE, Hitbox::HBData(hitbox_radius)) {
-        velocity.y = static_cast<float>(GetRandomValue(min_velocity_range, max_velocity_range));
+        // Calculate the random speed of the Enemy
+        velocity.x = static_cast<float>(GetRandomValue(min_speed_x_range, max_speed_x_range));
+        velocity.y = static_cast<float>(GetRandomValue(min_speed_y_range, max_speed_y_range));
+
+        // Pick a random direction [LEFT, STRAIGHT, RIGHT];
+        velocity.x *= GetRandomValue(-1, 1);
+        // TODO: Add Timer switch_dir(3.0f) and add logic to switch the x velocity on IsReady(); so the enemys have flight patterns
     }
 
     void Enemy::Draw() const {
@@ -21,10 +29,13 @@ namespace bts
 
     void Enemy::Update(float dt) {
         if (this->IsAlive()) {
-            position += Vector2UnitY * velocity * dt;
+            position += velocity * dt;
         }
 
         if (position.y >= HEIGHT + SPAWN_PADDING) {
+            this->Kill();
+        }
+        if (position.x >= WIDTH + SPAWN_PADDING || position.x <= -SPAWN_PADDING) {
             this->Kill();
         }
     }
